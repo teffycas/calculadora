21eff018e033983304e673673da0a90d4669b6a6
TEFFY <tifa_cp_pretty@hotmail.com>
2015-09-16 17:23:03 -0400

    he escrito mi menu de calculadora}

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -10,12 +10,36 @@ namespace calculadora
     {
         static void Main(string[] args)
         {
-            int n;
+            int n, op;
 
             Console.WriteLine("HOLA MUNDO");
             Console.ReadKey();
-            Console.WriteLine("INGRESE UN NUMERO");
+            Console.WriteLine("INGRESE PRIMER NUMERO");
             n = int.Parse(Console.ReadLine());
+            Console.WriteLine("INGRESE SEGUNDO NUMERO");
+            n = int.Parse(Console.ReadLine());
+
+            
+            do
+            {
+                Console.WriteLine("1. SUMA");
+                Console.WriteLine("2. RESTA");
+                Console.WriteLine("3. MULTIPLICACION");
+                
+                switch (op)
+                {
+                    case 1:
+                        { 
+                        }
+                    case 2:
+                        { }
+                    case 3:
+                        {}
+                
+ 
+                }
+
+            }while(Op!=4);
 
         }
     }
