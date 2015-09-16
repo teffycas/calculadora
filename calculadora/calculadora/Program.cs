e6ed016a798d4817a90231af1c8f52be94dcc982
TEFFY <tifa_cp_pretty@hotmail.com>
2015-09-16 18:28:17 -0400

    he unido la resta y la suma

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -39,13 +39,13 @@ namespace calculadora
                         }
                     case 2:
                         {
-<<<<<<< HEAD
+
 
                             int r = n - n1;
                             Console.WriteLine("la resta es:"+r);
-=======
+
                             
->>>>>>> suma
+
                             break;
                         }
                     case 3:
